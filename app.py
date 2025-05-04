a5619b28f4383fbdf82de77e9707aeb9cb26a8b2
Essraa <161943448+essraaadel@users.noreply.github.com>
2025-05-04 15:39:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import joblib
 import numpy as np
-
+import pickle
 # Load model and label encoders
-model = joblib.load("random_forest_model.joblib")
+model = pickle.load("random_forest_model.pkl")
 rain_today_encoder = joblib.load("RainToday_LabelEncoder.joblib")
 location_encoder = joblib.load("Location_LabelEncoder.joblib")
 wind_dir_encoder = joblib.load("WindDir3pm_LabelEncoder.joblib")
